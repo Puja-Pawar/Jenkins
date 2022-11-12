8b0bfbfd83ee8bccd607f92bd994bd75def0dcbd
Puja <pujasantoshp@gmail.com>
2022-11-12 08:24:38 +0530

    Welcome to Github

---
+++ src/main/java/com/example/demo/StsWebDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsWebDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsWebDevProjectApplication.class, args);
+	}
+
+}
